672ea68dcf8b6dae03a210752af0d3558bc60188
Michael Provost <mprovost129@gmail.com>
2026-05-06 12:25:17 -0400

    storages

--- config/Settings/prod.py
+++ config/Settings/prod.py
@@ -20,7 +20,7 @@ STORAGES = {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.WhiteNoiseStorage',
     },
 }
 
